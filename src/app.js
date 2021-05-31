c9b9dc1c947da905bf62e75850d20ecd0b1de59f
earthbound-misfit <79942028+earthbound-misfit@users.noreply.github.com>
2021-05-31 16:33:03 -0500

    Added axios and displayTemp function

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
-function displayTemperature(response) {
-    console.log(response.data);
+function displayTemp(response) {
+    console.log(response);
 }
 
 let apiKey = "dff2474c7e34531595f0b5e8d1de3e52";
 let apiUrl = `api.openweathermap.org/data/2.5/weather?q=Chicago&appid=${apiKey}&units=imperial`;
 
-axios.get(apiUrl).then(displayTemperature);
+axios.get(apiUrl).then(displayTemp);
