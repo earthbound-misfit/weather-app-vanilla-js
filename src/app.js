f62853e170ac66f9c64950a2c38aed6bf00e8f4a
earthbound-misfit <79942028+earthbound-misfit@users.noreply.github.com>
2021-05-31 17:38:08 -0500

    Added displayWeather function

--- src/app.js
+++ src/app.js
@@ -1,9 +1,21 @@
 function displayWeather(response) {
-        console.log(response.data);
+    console.log(response.data);
+        let temperatureElement = document.querySelector("#temperature");
+        let cityElement = document.querySelector("#city");
+        let descriptionElement = document.querySelector("#description");
+        let humidityElement = document.querySelector("#humidity");
+        let windElement = document.querySelector("#wind-speed");
+        
+        temperatureElement.innerHTML = Math.round(response.data.main.temp);
+        cityElement.innerHTML = response.data.name;
+        descriptionElement.innerHTML = response.data.weather[0].description;
+        humidityElement.innerHTML = response.data.main.humidity;
+        windElement.innerHTML = response.data.wind.speed;
+        
       }
 
-      let apiKey = "dff2474c7e34531595f0b5e8d1de3e52";
-      let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}`;
+let apiKey = "dff2474c7e34531595f0b5e8d1de3e52";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Chicago&appid=${apiKey}&units=imperial`;
 
-      axios.get(apiUrl).then(displayWeather);
+axios.get(apiUrl).then(displayWeather);
 
