3b16fdae6edbf1494c5c5dcfb46ad7410611a184
earthbound-misfit <79942028+earthbound-misfit@users.noreply.github.com>
2021-05-31 18:35:51 -0500

    icon and alt attributes

--- src/app.js
+++ src/app.js
@@ -26,6 +26,9 @@ function displayWeather(response) {
         let humidityElement = document.querySelector("#humidity");
         let windElement = document.querySelector("#wind-speed");
         let dateElement = document.querySelector("#date");
+        let iconElement = document.querySelector("#icon");
+        let icon = response.data.weather[0].icon;
+        let altElement = document.querySelector("#icon");
         
         temperatureElement.innerHTML = Math.round(response.data.main.temp);
         cityElement.innerHTML = response.data.name;
@@ -33,10 +36,13 @@ function displayWeather(response) {
         humidityElement.innerHTML = response.data.main.humidity;
         windElement.innerHTML = response.data.wind.speed;
         dateElement.innerHTML = formatDate(response.data.dt * 1000);
+        iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${icon}@2x.png`);
+        iconElement.setAttribute("alt", response.data.weather[0].description);
       }
 
 let apiKey = "dff2474c7e34531595f0b5e8d1de3e52";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Chicago&appid=${apiKey}&units=imperial`;
+let city = "Los Angeles";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
 
 axios.get(apiUrl).then(displayWeather);
 
