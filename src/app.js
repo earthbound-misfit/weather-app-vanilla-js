cf230f7cce14ded782ccd240e06b7d784d667a80
earthbound-misfit <79942028+earthbound-misfit@users.noreply.github.com>
2021-05-31 18:10:47 -0500

    Updated time and date

--- src/app.js
+++ src/app.js
@@ -1,3 +1,23 @@
+function formatDate(timestamp) {
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+    ];
+    let day = days[date.getDay()];
+    return `${day} ${hours}:${minutes}`;
+}
+
 function displayWeather(response) {
     console.log(response.data);
         let temperatureElement = document.querySelector("#temperature");
@@ -5,13 +25,14 @@ function displayWeather(response) {
         let descriptionElement = document.querySelector("#description");
         let humidityElement = document.querySelector("#humidity");
         let windElement = document.querySelector("#wind-speed");
+        let dateElement = document.querySelector("#date");
         
         temperatureElement.innerHTML = Math.round(response.data.main.temp);
         cityElement.innerHTML = response.data.name;
         descriptionElement.innerHTML = response.data.weather[0].description;
         humidityElement.innerHTML = response.data.main.humidity;
         windElement.innerHTML = response.data.wind.speed;
-        
+        dateElement.innerHTML = formatDate(response.data.dt * 1000);
       }
 
 let apiKey = "dff2474c7e34531595f0b5e8d1de3e52";
