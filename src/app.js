27a98f65fbe41f7a18d3a8ceb1a3f0a124a565e0
earthbound-misfit <79942028+earthbound-misfit@users.noreply.github.com>
2021-05-31 16:45:15 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,6 +3,6 @@ function displayTemp(response) {
 }
 
 let apiKey = "dff2474c7e34531595f0b5e8d1de3e52";
-let apiUrl = `api.openweathermap.org/data/2.5/weather?q=Chicago&appid=${apiKey}&units=imperial`;
+let apiUrl = `api.openweathermap.org/data/2.5/weather?q=Chicago&appid=${apiKey}`;
 
 axios.get(apiUrl).then(displayTemp);
